161306dba5362f7bf3549b83928ebed02ab37148
Nathan Shirley <natethegreat2525@gmail.com>
2015-10-24 01:57:39 -0400

    wrote helper functions

--- SeamCarving/src/com/seamcarving/SeamCarving.java
+++ SeamCarving/src/com/seamcarving/SeamCarving.java
@@ -5,11 +5,23 @@ import processing.core.PImage;
 public class SeamCarving {
 	
 	
-	public PImage carveSeam(PImage image){
+	public static PImage carveSeam(PImage image){
 		
 		
 		return image;
 		
+	}
+	
+	public static int getIdx(int x, int y, int width) {
+		return x + (y * width);
+	}
+	
+	public static int getX(int idx, int width) {
+		return idx % width;
+	}
+	
+	public static int getY(int idx, int width) {
+		return idx / width;
 	}
 
 }
