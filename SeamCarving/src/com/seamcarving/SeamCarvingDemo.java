c3c893523b6ffe1220de5509f1cf4fef13162c25
Nathan Shirley <natethegreat2525@gmail.com>
2015-10-27 19:55:45 -0400

    Fixed color to red

--- SeamCarving/src/com/seamcarving/SeamCarvingDemo.java
+++ SeamCarving/src/com/seamcarving/SeamCarvingDemo.java
@@ -23,9 +23,9 @@ public class SeamCarvingDemo extends PApplet {
 	
 	public void draw() {
 		background(0);
-		g.color(255, 255, 255);
+		g.fill(255, 255, 255);
 		g.image(image, 400, 300);
-		g.color(255, 0, 0, 255);
+		g.fill(255, 0, 0, 255);
 		if (SeamCarving.lastSeam != null) {
 			for (int i = 0; i < SeamCarving.lastSeam.length; i++) {
 				g.rect(SeamCarving.lastSeam[i] + 400 - image.width/2, i + 300 - image.height/2, 2, 2);
